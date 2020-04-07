69fab046a438d8a741d95621338cd8443f8e3832
Zach Colbert <colbertz@oregonstate.edu>
2020-04-07 14:14:14 -0700

    Remove build directory
    Since I'm using OverLeaf now for editing/compiling, I no longer need the
    LaTex build stuff I was using before.

--- build/bat/latexdockerclean.bat
+++
@@ -1,3 +0,0 @@
-@ECHO OFF
-
-@docker run --rm -i --net=none -v %CD%:/data blang/latex:ctanfull latexmk -cd -c %1
--- build/bat/latexdockerinteractive.bat
+++
@@ -1,3 +0,0 @@
-@ECHO OFF
-
-@docker run --rm -it --net=none -v %CD%:/data blang/latex:ctanfull bash
